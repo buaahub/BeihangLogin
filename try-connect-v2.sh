eb4f7ae81b139d74ae1612453f7a77f73e6e2f2f
sephirah <285529217@qq.com>
2022-03-02 19:52:28 +0800

    follow redirection and keep cookie when accessing GateWay API

--- try-connect-v2.sh
+++ try-connect-v2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-online=$(curl -sS "https://gw.buaa.edu.cn/cgi-bin/rad_user_info")
+COOKIEFILE=`mktemp`
+online=$(curl -sS -L -c $COOKIEFILE "https://gw.buaa.edu.cn/cgi-bin/rad_user_info")
+rm $COOKIEFILE
+
 grep "not_online_error" <<< $online > /dev/null
 if [[ "$?" != "0" ]]; then
         echo online: $(cut -d, -f1 <<< $online)
